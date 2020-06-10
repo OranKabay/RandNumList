a0fe6744ff738f8042d2e22f64ee0305420181e9
Oran <oran@emetrix.biz>
2020-06-10 13:53:34 +0100

    Fun

--- RandomNumberList.py
+++ RandomNumberList.py
@@ -1,6 +1,11 @@
 import random
-list = []
+x = []
 for i in range(100):
-    list.append(random.randrange(1,101,1))
-    list.sort()
-    print(list)  
+    x.append(random.randrange(1,101,1))
+print(x)
+x.sort()
+print("\n")
+print(x)
+x = list(dict.fromkeys(x))
+print("\n")
+print(x)
